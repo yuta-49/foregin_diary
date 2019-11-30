693e39a6fa52d1042162c51908e6820317066b8f
yuta-49 <yumon.f2549@gmail.com>
2019-11-30 22:10:37 +0900

    make-message/new

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,22 @@
 class MessagesController < ApplicationController
 
   def index
-
+    @messages = Message.all
   end
 
   def  new
+    @message = Message.new
   end
 
   def create
+    Message.create(new_params)
   end
 
-  
+
+
+  private
+  def new_params
+    params.require(:message).permit(:good, :bad, :improvement)
+  end
+
 end
