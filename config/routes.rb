6954593a03fd107936dcbc28ceafd741f904bc1b
yuta-49 <yumon.f2549@gmail.com>
2019-12-01 01:28:12 +0900

    make-user-show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
   root to: "messages#index"
   resources :messages, only: [:index, :new, :create, :show]
   resources :datas, only: [:index, :new, :create]
+  resources :users, only: [:show]
 end
+
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_30_144543) do
+ActiveRecord::Schema.define(version: 2019_11_30_160330) do
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "good"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_11_30_144543) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
