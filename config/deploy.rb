e2c593cfbdb98d767aa9a8baeb27bfd96d35771b
yuta-49 <yumon.f2549@gmail.com>
2019-12-12 17:45:22 +0900

    .bundle-chnage/config

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :repo_url,  'git@github.com:yuta-49/foregin_diary.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1' 
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
