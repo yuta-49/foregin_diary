278635ac2b7185e60cc3821e575d1c84dd5947af
yuta-49 <yumon.f2549@gmail.com>
2019-12-17 13:23:49 +0900

    again-make-controller/calender

---
+++ app/controllers/calenders_controller.rb
@@ -0,0 +1,2 @@
+class CalendersController < ApplicationController
+end
---
+++ test/controllers/calenders_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CalendersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
