af0a430b4da378b378d7ba70bbe8b6459a04348b
yuta-49 <yumon.f2549@gmail.com>
2019-12-28 16:30:41 +0900

    message_controller_respec/delete

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -25,4 +25,37 @@ describe MessagesController, type: :controller do
       expect(response.status).to eq 302
     end
   end
+
+  describe 'Delete #destroy' do
+    before do
+      @user = FactoryBot.create(:user)
+      @message = @user.messages.create(
+        good: 'ほげ',
+        bad: 'ほげ',
+        improvement: 'ほげ',
+        month_id: 1,
+        day_id: 1,
+        user_id: 1
+      )
+    end
+
+    context "for existing users" do
+      it "request should be a 302 redirect" do
+        delete :destroy, params: {id: @message.user.id}
+        expect(response.status).to eq 302
+      end
+
+      # it "renders the :new template" do
+      #   delefte :destroy, params: {id: @message.user.id}
+      #   expect(response).to redirect_to :new
+      # end
+    end
+    context "how to delete a message" do
+
+      it "deletes the message" do
+        delete :destroy, params: {id: @message.user.id}
+        expect{ response }.to change(Message,:count).by(-1)
+      end
+    end
+  end
 end
