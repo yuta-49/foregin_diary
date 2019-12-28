ce5fb9b04d780ab424b42c667247e124a9917642
yuta-49 <yumon.f2549@gmail.com>
2019-12-28 14:56:03 +0900

    messages_controller_rspec/create

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -7,5 +7,10 @@ describe MessagesController, type: :controller do
       get :index
       expect(response).to be_success
     end
+    
+    it "renders the :create template" do
+      post :create
+      expect(response).to redirect_to root_path
+    end
   end
 end
