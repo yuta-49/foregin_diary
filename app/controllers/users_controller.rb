57b20120ba55df375ae85175415ef7f16aabecfd
yuta-49 <yumon.f2549@gmail.com>
2020-01-01 23:11:15 +0900

    search-page-edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @nickname = current_user.nickname 
     @messages = current_user.messages.page(params[:page]).per(4)
     if 
-      @users = current_user.messages.where(['good LIKE ?', "%#{params[:good]}%"]) #検索とuseanameの部分一致を表示。
+      @users = current_user.messages.where(['good LIKE ?', "%#{params[:good]}%"]).page(params[:page]).per(4) #検索とuseanameの部分一致を表示。
     else
       @users = User.none
     end
