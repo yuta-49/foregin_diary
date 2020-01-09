e6c85b9936bc5e2a1df46dfbcada909dd62eb50d
yuta-49 <yumon.f2549@gmail.com>
2020-01-09 14:00:35 +0900

    user.rb find_auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable, :omniauthable
 
-  devise :omniauthable, :omniauth_providers => [:facebook]
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+  
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    User.dummy_email(auth),
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+  
+    user
+  end
+  
+  private
+  
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
   def self.search(params)
     results = all.order(created_at: :desc)
     results = results.where('good LIKE ?', "%#{params[:search]}%") if params[:search].present?
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,7 +259,6 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :facebook, Rails.application.credentials.facebook[:app_id], Rails.application.credentials.facebook[:app_secret]
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
   # ==> Warden configuration
