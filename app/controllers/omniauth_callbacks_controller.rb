0b882a0ea6ac63d6c20fa4aec2777acbe43b1bea
yuta-49 <yumon.f2549@gmail.com>
2020-01-09 15:04:44 +0900

    omniauth_controller-change

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,11 +2,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     callback_from :facebook
   end
- 
+
   private
   def callback_from(provider)
     provider = provider.to_s
 
+    # userモデルに定義したfinde_for_oauthメソッドを使用
     @user = User.find_for_oauth(request.env['omniauth.auth'])
 
     if @user.persisted?
