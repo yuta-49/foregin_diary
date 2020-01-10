c9a11fa9f13c33daf6387dbc4c345cc30673ec83
yuta-49 <yumon.f2549@gmail.com>
2020-01-10 14:02:24 +0900

    users rails db  migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_17_144616) do
+ActiveRecord::Schema.define(version: 2020_01_10_050119) do
 
   create_table "calenders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "data_win", null: false
@@ -75,6 +75,9 @@ ActiveRecord::Schema.define(version: 2019_12_17_144616) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
+    t.string "uid"
+    t.string "provider"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
