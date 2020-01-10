66ac019ab3a91ec327b4657bb3799fdb91b21355
yuta-49 <yumon.f2549@gmail.com>
2020-01-10 14:17:30 +0900

    user.rb private email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable, :omniauthable
-  
-  def self.find_for_oauth(auth)
-    user = User.where(uid: auth.uid, provider: auth.provider).first
-
-    unless user
-      user = User.create(
-        uid:      auth.uid,
-        provider: auth.provider,
-        email:    auth.info.email,
-        name:  auth.info.name,
-        password: Devise.friendly_token[0, 20],
-      )
-    end
-
-    user
-  end
 
   def self.search(params)
     results = all.order(created_at: :desc)
@@ -35,5 +19,27 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :password, presence: true
   validates :nickname, presence: true, length: { maximum: 6 }
+  
 
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    auth.info.email,
+        name:  auth.info.name,
+        password: Devise.friendly_token[0, 20],
+      )
+    end
+
+    user
+  end
+
+  private
+
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
 end
