9495ff297d4438b10124e3730c8177e50c26ef0a
yuta-49 <yumon.f2549@gmail.com>
2020-01-10 14:07:06 +0900

    routes collback_omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   
   root to: "messages#index"
   resources :messages, only: [:index, :new, :create, :show, :edit, :destroy, :update]
