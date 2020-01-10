7dc0ae746fb7f0138e4e440d1051550e3ec5ebad
yuta-49 <yumon.f2549@gmail.com>
2020-01-10 23:47:55 +0900

    google sns_credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable, 
-          :omniauthable, :omniauth_providers: %i[facebook google_oauth2]
+          :recoverable, :rememberable, :validatable, 
+          :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
 
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,4 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
-  provider :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'],
-  {:provider_ignores_state => true}
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
   
-end
+# end
