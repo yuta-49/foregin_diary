63a78f6726793e5cc94bca9e542ba7189057cab2
yuta-49 <yumon.f2549@gmail.com>
2020-01-10 20:41:06 +0900

    delete user_clomun

--- db/migrate/20191217144616_calender.rb
+++
@@ -1,7 +0,0 @@
-class Calender < ActiveRecord::Migration[5.2]
-  def change
-    add_column :calenders, :title, :string
-    add_column :calenders, :start, :datetime 
-    add_column :calenders, :end, :datetime 
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_10_050119) do
+ActiveRecord::Schema.define(version: 2020_01_10_110213) do
 
   create_table "calenders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "data_win", null: false
@@ -61,6 +61,15 @@ ActiveRecord::Schema.define(version: 2020_01_10_050119) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -75,11 +84,9 @@ ActiveRecord::Schema.define(version: 2020_01_10_050119) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
-    t.string "uid"
-    t.string "provider"
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "sns_credentials", "users"
 end
