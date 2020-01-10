85d767fd26bb0bc2de9657563e197406af0c07ad
yuta-49 <yumon.f2549@gmail.com>
2020-01-10 14:00:51 +0900

    user.rb omniauthable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable
+          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
 
   def self.search(params)
