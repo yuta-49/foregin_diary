d8b498b4b363d4cdcbb51221bdc2d7ee618293fc
yuta-49 <yumon.f2549@gmail.com>
2020-01-10 22:20:38 +0900

    user.rb google

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable, 
-          :omniauthable, :omniauth_providers => [:facebook]
+          :omniauthable, :omniauth_providers: %i[facebook google_oauth2]
 
 
 
