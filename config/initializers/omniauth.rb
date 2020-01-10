12a4b8e64396caa92130d010c4e81ba69efdd2d1
yuta-49 <yumon.f2549@gmail.com>
2020-01-10 22:25:12 +0900

    omniauth google

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
+  provider :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'],
   {:provider_ignores_state => true}
   
 end
