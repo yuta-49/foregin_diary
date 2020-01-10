93c460083cb828d362c45caa02e51e1b9affcf79
yuta-49 <yumon.f2549@gmail.com>
2020-01-10 20:54:38 +0900

    omniauth sns_credential

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,23 +3,23 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     callback_from :facebook
   end
 
-  
+
   def callback_from(provider)
-    provider = provider.to_s
 
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-
-    if @user.persisted?
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+    info = User.find_oauth(request.env["omniauth.auth"])
+    @user = info[:user]
+    sns_id = info[:sns_id]
+    if @user.persisted? #userがsave済みであれば
       sign_in_and_redirect @user, event: :authentication
-    else
-      session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
+      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+    else #userがsaveされてないインスタンスの場合は
+      session["devise.sns_id"] = sns_id
+      @password = Devise.friendly_token[0,20]
+      render registration_sns_signup_index_path
     end
   end
-
-
-  # def failure
-  #   redirect_to root_path
-  # end
+  
+  def failure
+    redirect_to root_path and return
+  end
 end
