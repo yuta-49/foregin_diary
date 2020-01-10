7f1bba0199f3c6e1359d21a1a87d5cdb77b3b72b
yuta-49 <yumon.f2549@gmail.com>
2020-01-10 14:05:11 +0900

    user.rb add omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
 
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    auth.info.email,
+        name:  auth.info.name,
+        password: Devise.friendly_token[0, 20],
+      )
+    end
+
+    user
+  end
 
   def self.search(params)
     results = all.order(created_at: :desc)
